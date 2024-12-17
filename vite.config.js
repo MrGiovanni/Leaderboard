5cdfea8c2a7795101c418ebac81830faa29510af
ZeyuZhao-git <zhaozeyu9185@gmail.com>
2024-12-16 20:48:39 -0500

    try1

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: './',
+  base: '/TouchStone_visualization/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
